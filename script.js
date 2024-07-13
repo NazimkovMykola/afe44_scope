42c2d44e9260a2aab5da112489192dfd0e2b8dc8
Mykola <148981382+NazimkovMykola@users.noreply.github.com>
2024-07-13 11:33:09 +0300

    i

--- script.js
+++ script.js
@@ -3,78 +3,74 @@
 // {}
 
 //      глобальною
-const userName = "Mykola"
+const userName = "Mykola";
 // локальні
 
 function sayName() {
-  const x = 5
-  console.log(x)
+  const x = 5;
+  console.log(x);
 }
 const user = {
   text: userName,
   func: () => {
-    const y = 10
-    console.log(y)
+    const y = 10;
+    console.log(y);
   },
-}
-const arr = [userName]
-sayName()
+};
+const arr = [userName];
+sayName();
 
-const z = 10
+const z = 10;
 
 function test1() {
-  console.log(z)
+  console.log(z);
 }
-test1()
+test1();
 
 // лексичне оточення = {}/блок коду + змінні
 // глобальним та локальним
 
-const textName = "Mykola"
+const textName = "Mykola";
 function printName(textName) {
   // const textName = "Ivan"
-  console.log(textName)
+  console.log(textName);
 }
 
-printName("Ivan")
+printName("Ivan");
 
 // ключових слів let та const
 // var - завжди має глобально область видимості
 // код пишеться один раз
 // а читаєтся 1000 разів
 
-//чисті функції 
+//чисті функції
 
+function нагітиВоду() {}
+function взятиЧашку() {}
+function змішатиВсеВЧашці() {}
 
-
-function нагітиВоду () {}
-function взятиЧашку () {}
-function змішатиВсеВЧашці () {}
-
-нагітиВоду()
-взятиЧашку()
-змішатиВсеВЧашці()
-
+нагітиВоду();
+взятиЧашку();
+змішатиВсеВЧашці();
 
 // console.log(tag)
 
-// об'єктно орієнтоване програмування ООП 
-
+// об'єктно орієнтоване програмування ООП
 
 function drawTag(tagName) {
-  const tag = document.createElement(tagName)
-  tag.textContent = "Я - " + tagName
-  document.body.appendChild(tag)
+  const tag = document.createElement(tagName);
+  tag.textContent = "Я - " + tagName;
+  document.body.appendChild(tag);
 }
 
-drawTag("span")
-drawTag("div")
+drawTag("span");
+drawTag("div");
 
 // функція-конструктор об'єту
 
 // function User (name, age, isStudent) {
 //     this.name = name,
-//     this.age = age, 
+//     this.age = age,
 //     this.isStudent=  isStudent
 // }
 // const users = []
@@ -84,23 +80,22 @@ drawTag("div")
 
 // console.log(users)
 
-function Tag (tagName, i) {
-    this.tagName = tagName,
-    this.draw = () => {
-          const tag = document.createElement(this.tagName)
-          const gameField = document.querySelector(".gameField")
-          tag.classList.add('cell')
-          tag.textContent = i
-          gameField.appendChild(tag)
-    }
+function Tag(tagName, i) {
+  (this.tagName = tagName),
+    (this.draw = () => {
+      const tag = document.createElement(this.tagName);
+      const gameField = document.querySelector(".gameField");
+      tag.classList.add("cell");
+      tag.textContent = i;
+      gameField.appendChild(tag);
+    });
 }
 
-const newTagDiv = new Tag('div')
-newTagDiv.draw()
-
+const newTagDiv = new Tag("div");
+newTagDiv.draw();
 
 //змійка
-for(let i =0; i<99; i++) {
-    new Tag('div',i).draw()
-}
 
+for (let i = 0; i < 99; i++) {
+  new Tag("div", i).draw();
+}
